833a3bc159769d7057e9a488739b15a25fe59d0e
刘庆林 <1459307172@qq.com>
2024-08-29 10:01:37 +0800

    init: a simple ts progrom

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development", // 设置 mode 选项为 'development' 或 'production'
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
